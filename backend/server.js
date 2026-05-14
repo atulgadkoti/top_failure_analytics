d9c5bf3f50a4fc4184e03c4021dfde158a5e22ee
AtulGadkoti <atulgadkoti@gmail.com>
2026-05-14 09:45:39 +0530

    updated server.js

--- backend/server.js
+++ backend/server.js
@@ -10,23 +10,22 @@ app.use(express.json());
 app.get("/api/analytics/top-failures/:customer_id", async (req,res)=>{
   const custID = req.params.customer_id;
   try {
-    
-    const data = await pool.query(
-      `
-      SELECT 
-      c.customer_name, 
-      t.failure_category, 
-      COUNT(*) AS ticket_count
-      FROM tickets t
-      JOIN customers c 
-      ON t.customer_id = c.customer_id
-      WHERE t.customer_id = $1
-      AND t.resolved = FALSE
-      AND t.failure_category IS NOT NULL
-      GROUP BY c.customer_name, t.failure_category
-      ORDER BY ticket_count DESC
-      LIMIT 3;
-      `,
+      const data = await pool.query(
+        `
+            SELECT 
+            c.customer_name, 
+            t.failure_category, 
+            COUNT(*) AS ticket_count
+            FROM tickets t
+            JOIN customers c 
+            ON t.customer_id = c.customer_id
+            WHERE t.customer_id = $1
+            AND t.resolved = FALSE
+            AND t.failure_category IS NOT NULL
+            GROUP BY c.customer_name, t.failure_category
+            ORDER BY ticket_count DESC
+            LIMIT 3;
+        `,
       [custID]
     );
 
@@ -38,7 +37,7 @@ app.get("/api/analytics/top-failures/:customer_id", async (req,res)=>{
     console.error(error);
 
     res.status(500).json({
-      error: "Internal Server Error",
+      error:"Internal Server Error",
     }
   );
   }
